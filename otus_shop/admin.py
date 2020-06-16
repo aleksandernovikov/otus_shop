69aee2cfa20ef17b2056def5864070304bec1f4e
user <79fz270704@gmail.com>
2020-06-16 18:19:27 +0300

    wip

--- otus_shop/admin.py
+++ otus_shop/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
 
-# Register your models here.
+from otus_shop.models import ProductCategory, Product, Cart
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(TreeAdmin):
+    form = movenodeform_factory(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_filter = 'user',
--- otus_shop/models.py
+++ otus_shop/models.py
@@ -1,24 +1,38 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from treebeard.mp_tree import MP_Node
 
 User = get_user_model()
 
 
-class ProductCategory(models.Model):
+class ProductCategory(MP_Node):
     """
     Категория товара
     """
     title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
 
 
 class Product(models.Model):
     """
     Товар
     """
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
 
 
 class Cart(models.Model):
+    """
+    Корзина
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     count = models.PositiveSmallIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.user} -> {self.product} x {self.count}'
