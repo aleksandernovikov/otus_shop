ec0211d84da3e6dde49adee28cec087585f0cd0d
user <79fz270704@gmail.com>
2020-06-16 23:16:20 +0300

    wip

--- otus_shop/admin.py
+++ otus_shop/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 from treebeard.admin import TreeAdmin
 from treebeard.forms import movenodeform_factory
 
-from otus_shop.models import ProductCategory, Product, Cart
+from otus_shop.models import ProductCategory, Product, Cart, Characteristic, ProductVariantCharacteristic, Measure, \
+    ProductVariant
+
+
+# class ProductCharacteristicInline(admin.TabularInline):
+#     model = ProductVariantCharacteristic
+#     extra = 1
+
+
+class ProductVariantInline(admin.TabularInline):
+    model = ProductVariant
+    extra = 1
 
 
 @admin.register(ProductCategory)
@@ -12,6 +23,17 @@ class ProductCategoryAdmin(TreeAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    inlines = ProductVariantInline,
+    pass
+
+
+@admin.register(Measure)
+class MeasureAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Characteristic)
+class CharacteristicAdmin(admin.ModelAdmin):
     pass
 
 
--- otus_shop/models.py
+++ otus_shop/models.py
@@ -10,9 +10,13 @@ class ProductCategory(MP_Node):
     Категория товара
     """
     title = models.CharField(max_length=100)
+    show_subcategory_products = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        unique_together = 'title', 'depth'
 
 
 class Product(models.Model):
@@ -24,6 +28,61 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ProductVariant(models.Model):
+    """
+    Вариант товара (размер, цвет, длина)
+    """
+    parent_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+
+    characteristics = models.ManyToManyField(
+        'Characteristic',
+        blank=True,
+        through='ProductVariantCharacteristic'
+    )
+
+    def __str__(self):
+        return f'{self.parent_product} -> {self.title}'
+
+
+class Measure(models.Model):
+    """
+    Единица измерения
+    """
+    title = models.CharField(max_length=50, unique=True)
+    short_title = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Characteristic(models.Model):
+    """
+    Характеристика товара
+    """
+    title = models.CharField(max_length=100, unique=True)
+    short_title = models.CharField(max_length=50, unique=True)
+    measure = models.ForeignKey(Measure, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class ProductVariantCharacteristic(models.Model):
+    """
+    Характеристики варианта конкретного товара
+    """
+    product_variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
+    characteristic = models.ForeignKey(Characteristic, on_delete=models.CASCADE)
+    value = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = 'product_variant', 'characteristic'
+
+    def __str__(self):
+        return f'{self.characteristic}: {self.value}'
 
 
 class Cart(models.Model):
