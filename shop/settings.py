680205b86288c3645c308af6d352e8e0808f0d8b
Alexander <79fz270704@gmail.com>
2021-07-06 13:49:09 +0300

    some fixes

--- shop/settings.py
+++ shop/settings.py
@@ -153,6 +153,8 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
